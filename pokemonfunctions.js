a3830700f9f999e2aaf5480416056e075a19d7f6
coolnick247 <nickkj51100@gmail.com>
2017-07-12 17:38:48 -0400

    Work

--- pokemonfunctions.js
+++ pokemonfunctions.js
@@ -1,20 +1,29 @@
 var letters = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
+var lettersLength = letters.length;
 function randomLetter(why){
     var randomIndex = Math.floor(Math.random() * why);
-    console.log(letters[randomIndex]);
     return letters[randomIndex];
 }
-randomLetter(27);
+console.log(randomLetter(lettersLength));
 
-function randWord(){
-var word = " ";
-for (var i = 0; i < 6, i++){
-    word = word + randomLetter();
-return word;
-}
-}
-randWord();
 
+
+function randWord()
+{
+    var word = "";
+    for (var i = 0; i <  6; i++)
+    {
+        word = word + randomLetter(lettersLength);
+    }
+    return word;
+}
+console.log(randWord());
+
+function randSentence()
+{
+   var sentense = console.log(randomWord() + " " + randomWord() + " " + randomWord());
+}
+
+console.log(randWord(sentense));
 
 
